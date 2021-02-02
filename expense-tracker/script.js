0676bba386d8eb416463b85c6a31190aa1b83073
johanawan <johanawan@hotmail.com>
2021-02-02 17:07:41 +0000

    Added balance, income and expense to dashboard

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -32,11 +32,36 @@ function addTransactionDOM(transaction) {
     list.appendChild(item)
 }
 
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    // Calculate total
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2)
+
+    // Expenses
+    const expense = (amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => (acc += item), 0) * -1)
+    .toFixed(2)
+
+    balance.innerText = `£${total}`
+    money_plus.innerText = `+£${income}`
+    money_minus.innerText = `-£${expense}`
+
+}
+
 // Init app
 function init() {
     list.innerHTML = "";
 
     transactions.forEach(addTransactionDOM)
+    updateValues();
 }
 
 init()
